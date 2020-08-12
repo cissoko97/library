eca195319998db621f0a71f82a1c60530805635b
davidm1806 <davidluiz.matjaba@gmail.com>
2020-08-12 10:35:08 +0200

    implementation of auto_update

---
+++ src/main/java/org/ckCoder/service/DownloadService.java
@@ -0,0 +1,100 @@
+package org.ckCoder.service;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+public class DownloadService extends Service<Boolean> {
+    /**
+     * The logger of the class
+     */
+    private final Logger logger = Logger.getLogger(DownloadService.class.getName());
+
+    // -----
+    private long totalBytes;
+    private boolean succeeded;
+    private volatile boolean stopThread;
+
+    private final ObjectProperty<URL> remoteResourceLocation = new SimpleObjectProperty<>();
+    private final ObjectProperty<Path> pathToLocalResource = new SimpleObjectProperty<>();
+
+    private Thread copyThread;
+
+    public DownloadService() {
+        setOnSucceeded(s -> {
+            System.out.println("Succeeded with value: " + super.getValue() + " , Copy Thread is Alive? " + copyThread.isAlive());
+            done();
+        });
+
+        setOnFailed(f -> {
+            System.out.println("Failed with value: " + super.getValue() + " , Copy Thread is Alive? " + copyThread.isAlive());
+            done();
+        });
+
+        setOnCancelled(c -> {
+            System.out.println("Cancelled with value: " + super.getValue() + " , Copy Thread is Alive? " + copyThread.isAlive());
+            done();
+        });
+    }
+
+    /**
+     * The Service is done
+     */
+    private boolean done() {
+
+        boolean fileDeleted = false;
+
+        //Check if The Service Succeeded
+        if (!succeeded)
+            fileDeleted = new File(pathToLocalResource.get().toString()).delete();
+
+        return fileDeleted;
+    }
+
+    @Override
+    protected Task<Boolean> createTask() {
+        return new Task<Boolean>() {
+            @Override
+            protected Boolean call() throws Exception {
+                succeeded = Boolean.TRUE;
+
+                File destinationFile = new File(pathToLocalResource.get().toString());
+
+                super.updateMessage("Connecting with server");
+                String failMessage;
+
+                try {
+                    URLConnection connection = remoteResourceLocation.get().openConnection();
+                    totalBytes = Long.parseLong(connection.getHeaderField("content-Length"));
+
+
+                } catch (Exception ex) {
+
+                }
+                return succeeded;
+            }
+        };
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+    }
+
+    @Override
+    protected void cancelled() {
+        super.cancelled();
+    }
+
+    @Override
+    protected void failed() {
+        super.failed();
+    }
+}
